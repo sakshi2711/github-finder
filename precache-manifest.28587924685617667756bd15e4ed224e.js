a9fa734978d95c2851fe96b954fe2dcd477eaac6
sakshi2711 <mail.sakshibindal@gmail.com>
2018-10-21 18:20:09 +0530

    Updates

---
+++ precache-manifest.28587924685617667756bd15e4ed224e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e285274f2d816ccebd8",
+    "url": "/github-finder/static/js/runtime~main.8e285274.js"
+  },
+  {
+    "revision": "562448df16561e303907",
+    "url": "/github-finder/static/js/main.562448df.chunk.js"
+  },
+  {
+    "revision": "0e91750f1de482f15ff9",
+    "url": "/github-finder/static/js/1.0e91750f.chunk.js"
+  },
+  {
+    "revision": "562448df16561e303907",
+    "url": "/github-finder/static/css/main.6e86a116.chunk.css"
+  },
+  {
+    "revision": "0e91750f1de482f15ff9",
+    "url": "/github-finder/static/css/1.35fa4295.chunk.css"
+  },
+  {
+    "revision": "6eae2834c0bb5a646c5d2d0fb094073d",
+    "url": "/github-finder/index.html"
+  }
+];
